b795be81a3e41d9f60e99fb45d140b23e02f01ab
S Selvakumar <skselvamca3@gmail.com>
2026-04-07 00:44:03 +0530

    Add files via upload

---
+++ autonomous_agent2.py
@@ -0,0 +1,58 @@
+import os
+from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+from tools import search_tool
+
+load_dotenv()
+
+llm = ChatGroq(
+    groq_api_key=GROQ_API_KEY,
+    model_name="llama-3.1-8b-instant"     # ⚡ super fast
+)
+
+def ask_llm(prompt):
+    return llm.invoke(prompt).content
+
+
+def autonomous_agent2(user_task, max_steps=5):
+    global memory
+
+    context = "\n".join(memory)
+
+    for step in range(max_steps):
+
+        prompt = f"""
+You are JARVIS AI.
+
+Task: {user_task}
+
+Memory:
+{context}
+
+Decide next step:
+- THINK
+- SEARCH
+- FINAL
+
+Format:
+ACTION:
+CONTENT:
+"""
+
+        response = ask_llm(prompt)
+
+        memory.append(response)
+        context += "\n" + response
+
+        if "SEARCH" in response:
+            query = response.split("CONTENT:")[-1].strip()
+            results = search_tool(query)
+            context += "\n".join(results)
+
+        if "FINAL" in response:
+            return {
+                "steps": memory,
+                "final": response
+            }
+
+    return {"steps": memory, "final": "Stopped"}
